865ba0ec7ebf9e51c3d51146d322a0ed4ce90795
Vinícius Veroneze <vinicius.veroneze@tagview.com.br>
2021-10-18 12:10:27 -0300

    feat: Refactor post builder

--- api/factories.js
+++ api/factories.js
@@ -27,7 +27,12 @@ const buildLocation = createFactory({
 const buildPost = createFactory({
   uuid: faker.datatype.uuid,
   user: buildUser,
-  photo: () => `https://source.unsplash.com/random/800x800?x=${faker.datatype.uuid()}`,
+  photo: (width = 800, height = 800) => {
+    const url = faker.image.unsplash.imageUrl(width, height);
+    const key = faker.datatype.uuid();
+
+    return `${url}?key=${key}`;
+  },
   created_at: () => {
     const random = faker.date.past();
     const date = `${random.getDate()}/${random.getMonth() + 1}/${random.getFullYear()}`;
